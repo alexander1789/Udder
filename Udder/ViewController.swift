1478c421adf04b7403eda399cf8cbd9db9c3ff9a
Alessandro Lo Verso <a.loverso@hotmail.it>
2022-03-12 10:18:25 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Alessandro Lo Verso on 12/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
